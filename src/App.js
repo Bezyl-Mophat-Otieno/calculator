198abf5583c596d34a9b571985d2cef5af43eb05
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2022-12-23 05:55:00 +0300

    second Commit for development.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+const [prevOperand,setPrevOperand] = useState([])
+
+const readInput = (e)=>{
+  let input = parseInt(e.target.innerText);
+  if(prevOperand.length>=0){
+    setPrevOperand([...prevOperand,input])
+  }
+}
+
+const readOperator = (e)=>{
+  let input = e.target.innerText;
+  if(prevOperand.length>=0){
+    setPrevOperand([...prevOperand,input])
+  }
+}
+const clearAllPrevOperands = ()=>{
+  setPrevOperand([])
+
+}
+const deletePrevOperand = ()=>{
+  let currArray = prevOperand
+  console.log(currArray)
+  prevOperand.pop()
+  console.log(currArray)
+  setPrevOperand([...prevOperand])
+}
+const results = ()=>{
+let arrayExpression = prevOperand;
+let mathExpression=parseInt(arrayExpression.toString())
+console.log(mathExpression)
+
+}
+
+const [currOperand,setCurrOperand] = useState(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator-grid'>
+    <div className='output'>
+      <div className='previous-operand'> {prevOperand} </div>
+      <div className='current-operand'>5423</div>
+    </div>
+    <button className='span-two' onClick={()=>clearAllPrevOperands()}> AC</button>
+    <button  className="btn" onClick={()=>deletePrevOperand()}> DEL</button>
+    <button onClick={(e)=>readOperator(e)}> /</button>
+    <button onClick={(e)=>readInput(e)}> 1</button>
+    <button onClick={(e)=>readInput(e)}> 2</button>
+    <button onClick={(e)=>readInput(e)}> 3</button>
+    <button onClick={(e)=>readOperator(e)}> *</button>
+    <button onClick={(e)=>readInput(e)}> 4</button>
+    <button onClick={(e)=>readInput(e)}> 5</button>
+    <button onClick={(e)=>readInput(e)}> 6</button>
+    <button onClick={(e)=>readOperator(e)}> +</button>
+    <button onClick={(e)=>readInput(e)}> 7</button>
+    <button onClick={(e)=>readInput(e)}> 8</button>
+    <button onClick={(e)=>readInput(e)}> 9</button>
+    <button onClick={(e)=>readOperator(e)}> -</button>
+    <button onClick={(e)=>readOperator(e)}> .</button>
+    <button onClick={(e)=>readInput(e)}> 0</button>
+    <button className='span-two'onClick={()=>results()}>=</button>
+
+
+
+
     </div>
   );
 }
