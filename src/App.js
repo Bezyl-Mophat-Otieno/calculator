19f6d4a2494708cefff576aafe1c67c52b6173fd
Bezyl Mophat Otieno <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2022-12-28 14:45:57 +0300

    Refactored calculator working using only 2 useState Hooks.

--- src/App.js
+++ src/App.js
@@ -1,65 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect} from 'react';
+import { cleanup } from '@testing-library/react';
 function App() {
-const [prevOperand,setPrevOperand] = useState([])
+  
 
-const readInput = (e)=>{
-  let input = parseInt(e.target.innerText);
-  if(prevOperand.length>=0){
-    setPrevOperand([...prevOperand,input])
-  }
+const clearEntireExpression=()=>{
+setValue("")
+setResults("")
+}
+const deleteExpression=()=>{
+  setValue(value.slice(0,-1))
 }
 
-const readOperator = (e)=>{
-  let input = e.target.innerText;
-  if(prevOperand.length>=0){
-    setPrevOperand([...prevOperand,input])
-  }
-}
-const clearAllPrevOperands = ()=>{
-  setPrevOperand([])
-
-}
-const deletePrevOperand = ()=>{
-  let currArray = prevOperand
-  console.log(currArray)
-  prevOperand.pop()
-  console.log(currArray)
-  setPrevOperand([...prevOperand])
-}
-const results = ()=>{
-let arrayExpression = prevOperand;
-let mathExpression=parseInt(arrayExpression.toString())
-console.log(mathExpression)
-
+const compute= ()=>{
+try {
+const res = eval(value)
+setResults(()=>res)
+} catch (error) {
+ 
+}  
 }
 
-const [currOperand,setCurrOperand] = useState(null)
+
+const[value,setValue]=useState("")
+const[results,setResults]=useState("")
+
   return (
     <div className='calculator-grid'>
     <div className='output'>
-      <div className='previous-operand'> {prevOperand} </div>
-      <div className='current-operand'>5423</div>
+      <div className='expression'>{value}</div>
+      <div className='results'>{results}</div>
     </div>
-    <button className='span-two' onClick={()=>clearAllPrevOperands()}> AC</button>
-    <button  className="btn" onClick={()=>deletePrevOperand()}> DEL</button>
-    <button onClick={(e)=>readOperator(e)}> /</button>
-    <button onClick={(e)=>readInput(e)}> 1</button>
-    <button onClick={(e)=>readInput(e)}> 2</button>
-    <button onClick={(e)=>readInput(e)}> 3</button>
-    <button onClick={(e)=>readOperator(e)}> *</button>
-    <button onClick={(e)=>readInput(e)}> 4</button>
-    <button onClick={(e)=>readInput(e)}> 5</button>
-    <button onClick={(e)=>readInput(e)}> 6</button>
-    <button onClick={(e)=>readOperator(e)}> +</button>
-    <button onClick={(e)=>readInput(e)}> 7</button>
-    <button onClick={(e)=>readInput(e)}> 8</button>
-    <button onClick={(e)=>readInput(e)}> 9</button>
-    <button onClick={(e)=>readOperator(e)}> -</button>
-    <button onClick={(e)=>readOperator(e)}> .</button>
-    <button onClick={(e)=>readInput(e)}> 0</button>
-    <button className='span-two'onClick={()=>results()}>=</button>
+    <button className='span-two' onClick={()=>clearEntireExpression()}> AC</button>
+    <button  className="btn" onClick={()=>deleteExpression()}> DEL</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="/">/</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="1">1</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="2">2</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="3">3</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="*">*</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="4">4</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="5">5</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="6">6</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="+">+</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="7">7</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="8">8</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="9">9</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="-">-</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value=".">.</button>
+    <button onClick={(e)=>setValue(value + e.target.value)} value="0">0</button>
+    <button className='span-two' onClick={()=>compute()} >=</button>
 
 
 
